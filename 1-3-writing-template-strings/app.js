0da10362b7c5ac84fee83a60bc65ee47926b504c
Myat Ko Ko <coderinsider1@gmail.com>
2021-03-24 12:36:19 +0630

    Writing template-strings

---
+++ 1-3-writing-template-strings/app.js
@@ -0,0 +1,10 @@
+function showTheName(firstName) {
+	console.log("Hello", firstName);
+}
+showTheName("Hsu");
+
+function createPersonalMail(fName, price) {
+	let shipping = 50;
+	console.log(`Hi ${fName}! Thank you! Total: $${price} Shipping: $${shipping}`);
+}
+createPersonalMail("Guy", 28);
